18c7bac0c7d959839ed540879a048103098e0cd2
Gabriela Tavares <gabriela.ttavares@gmail.com>
2024-07-31 13:21:43 -0300

    feat: add UpdateIngredientDto

--- src/ingredient/dto/create-ingredinet.dto.ts
+++ src/ingredient/dto/create-ingredinet.dto.ts
@@ -4,3 +4,10 @@ export class CreateIngredientDto {
   readonly totalContent: string;
   readonly measure: string;
 }
+
+export class UpdateIngredientDto {
+  readonly id: string;
+  readonly price?: number;
+  readonly totalContent?: string;
+  readonly measure?: string;
+}
